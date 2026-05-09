36db8b6a403a03c8922c3652c771f0de4958c582
Ragil Achmad <achmadhabibu@gmail.com>
2026-05-09 21:26:28 +0700

    feat: implement sitemap.xml route and update vercel rewrites for static resource serving

---
+++ client/app/routes/sitemap[.xml].tsx
@@ -0,0 +1,30 @@
+export async function loader() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://gg-achmad.vercel.app/</loc>
+    <lastmod>2026-05-09</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://gg-achmad.vercel.app/#about</loc>
+    <lastmod>2026-05-09</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://gg-achmad.vercel.app/#projects</loc>
+    <lastmod>2026-05-09</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+}
